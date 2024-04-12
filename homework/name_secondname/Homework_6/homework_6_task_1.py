15151e132bb5c6d3b0f705be496b01a9613fcced
Alko-Tester <re-serviss@yandex.ru>
2024-04-12 13:41:22 +0300

    Домашка к уроку 6 Задачки с циклами и переборами и опять-таки линтер(

--- homework/name_secondname/Homework_6/homework_6_task_1.py
+++ homework/name_secondname/Homework_6/homework_6_task_1.py
@@ -1,8 +1,9 @@
 # Задание №1
 # Напишите программу, которая добавляет ‘ing’ к словам (к каждому слову) в тексте
 
-strings = ('Etiam tincidunt neque erat, quis molestie enim imperdiet vel. '
-            'Integer urna nisl, facilisis vitae semper at, dignissim vitae libero'
+strings = (
+    "Etiam tincidunt neque erat, quis molestie enim imperdiet vel. "
+    "Integer urna nisl, facilisis vitae semper at, dignissim vitae libero"
 )
 
 # Делим строку на список подстрок, разделитель - пробел
@@ -13,12 +14,12 @@ new_string = []
 # Проверяем подстроки на наличие точки и запятой, в зависимости от этого
 # определяем место для вставки
 for word in split_string:
-    if word[-1] in ',.':
-        word = word[:-1] + 'ing' + word[-1]
+    if word[-1] in ",.":
+        word = word[:-1] + "ing" + word[-1]
     else:
-        word += 'ing'
+        word += "ing"
     new_string.append(word)
 
 # Объединяем элементы списка в строку, разделитель - пробел
-new_string = ' '.join(new_string)
+new_string = " ".join(new_string)
 print(new_string)
