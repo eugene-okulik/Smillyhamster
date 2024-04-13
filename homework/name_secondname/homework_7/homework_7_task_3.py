cc6a1d10e70cb683bf8a051834cf81a339008023
Alko-Tester <re-serviss@yandex.ru>
2024-04-13 17:37:34 +0300

    Домашка к уроку 7 И снова линтер - обрамление функции пустыми строками

--- homework/name_secondname/homework_7/homework_7_task_3.py
+++ homework/name_secondname/homework_7/homework_7_task_3.py
@@ -7,10 +7,12 @@ out2 = "результат операции: 514"
 out3 = "результат работы программы: 9"
 out4 = "результат: 2"
 
+
 # поместил решение ДЗ  №5 в функцию
 def edit_string(some_string):
     out1_num = int(some_string[some_string.index(":") + 1:].strip())
     print(out1_num + 10)
+
 
 edit_string(out1)
 edit_string(out2)
