cfe2d2b232d47213f97873935f43b30c476fed9e
Alko-Tester <re-serviss@yandex.ru>
2024-04-27 13:02:04 +0300

    Домашка к уроку 9 Форматирование даты-времени Фильтрация и выборка из списка

---
+++ homework/name_secondname/homework_9/homework_9_task_1.py
@@ -0,0 +1,17 @@
+# Задача 1
+# Дана такая дата: "Jan 15, 2023 - 12:05:33"
+# Преобразуйте эту дату в питоновский формат, после этого:
+# 1. Распечатайте полное название месяца из этой даты
+# 2. Распечатайте дату в таком формате: "15.01.2023, 12:05"
+
+import datetime
+
+task_time = "Jan 15, 2023 - 12:05:33"
+
+new_task_time = datetime.datetime.strptime(task_time, "%b %d, %Y - %H:%M:%S")
+
+month = datetime.datetime.strftime(new_task_time, "%B")
+print(f"Полное название месяца {month}")
+
+formated_data = datetime.datetime.strftime(new_task_time, "%d.%m.%Y, %H:%M")
+print(f"Отформатированная дата {formated_data}")
